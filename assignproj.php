83ca89303e0a4ac44991c3ce04191be6cd6c0627
mNz09 <39593946+mNz09@users.noreply.github.com>
2020-10-14 00:23:30 +1100

    Add files via upload

---
+++ assignproj.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assign Projects</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta http-equiv="Cache-control" content="no-cache">
+</head>
+
+<body>
+	<div class="header">
+		<img src="images/logo.png" alt="logo here">
+		<!-- <img src="logo.png" alt="logohere"> -->
+
+	<nav id="navbar">
+			<ul>
+				<li><a class="navmenu" href="adminhome.php" >Home</a></li>
+				<li><a class="navmenu" href="register.php" >Add Employee</a></li>
+				<li><a class="navmenu" href="viewuser.php" >View Employee</a></li>
+				<li><a class="navmenu" id="current" href="assignproj.php" >Assign Projects</a></li>
+				<li><a class="navmenu" href="projstat.php" >Project Status</a></li>
+				<li><a class="navmenu" href="index.php" >Logout</a></li>
+			</ul>
+	</nav>
+	</div>
+	<footer class="footer">
+		<br>
+		<h5>Copyright &copy; 2020, Arif Systems. Created by TeamGenius</h5>
+		<br>
+	</footer>
+</body>
+</html>
---
+++ projstat.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project Status</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta http-equiv="Cache-control" content="no-cache">
+</head>
+
+<body>
+	<div class="header">
+		<img src="images/logo.png" alt="logo here">
+		<!-- <img src="logo.png" alt="logohere"> -->
+
+	<nav id="navbar">
+			<ul>
+				<li><a class="navmenu" href="adminhome.php" >Home</a></li>
+				<li><a class="navmenu" href="register.php" >Add Employee</a></li>
+				<li><a class="navmenu" href="viewuser.php" >View Employee</a></li>
+				<li><a class="navmenu" href="assignproj.php" >Assign Projects</a></li>
+				<li><a class="navmenu" id="current" href="projstat.php" >Project Status</a></li>
+				<li><a class="navmenu" href="index.php" >Logout</a></li>
+			</ul>
+		
+	</nav>
+	</div>
+	<footer class="footer">
+		<br>
+		<h5>
+		Email Us: info@arifsystems.org
+		</h5>
+		<h5>
+			Learn More About Us <a class="footerlink" href="about.php">(here)</a> <br>
+		</h5>
+		<p>
+		Copyright &copy; 2020, Arif Systems. Created by TeamGenius</p>
+		<br>
+		
+	</footer>
+</body>
+</html>
